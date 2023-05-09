6b444c19bb0853e008bcdad5e87248a7d313af1b
Nick Sacco <nicksacco10@gmail.com>
2023-05-08 19:55:15 -0700

    added shops show route, show controller action, and setup view

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,4 +2,8 @@ class ShopsController < ApplicationController
   def index
     @shops = Shop.all
   end
+
+  def show
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/shops', to: 'shops#index'
+  get '/shops/:id', to: 'shops#show'
 end
