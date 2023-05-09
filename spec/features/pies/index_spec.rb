47b3f427e25039c0d96f1edb1ee48fd573c66863
Nick Sacco <nicksacco10@gmail.com>
2023-05-09 15:42:48 -0700

    test: pies index tests and updated routes

--- spec/features/pies/index_spec.rb
+++ spec/features/pies/index_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe "Pies Index Page", type: :feature do
   describe "When I visit the Pie index page I see every pie and the following:" do
     it 'shows all pies and their attributes' do
       visit '/pies'
+
+      within "#pies_#{@mince.id}" do
+        expect(page).to have_content("Name: #{@mince.name}")
+        expect(page).to have_content("Category: #{@mince.category}")
+        expect(page).to have_content("Wholesale?: #{@mince.wholesale}")
+        expect(page).to have_content("Bake Time: #{@mince.bake_time}")
+      end
     end
   end
 end
