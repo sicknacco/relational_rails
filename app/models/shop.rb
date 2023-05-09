bcf8fc76c2fbc75af489256e377aa85d2caa7b09
Nick Sacco <nicksacco10@gmail.com>
2023-05-08 19:13:01 -0700

    Added shop validations

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,4 @@
 class Shop < ApplicationRecord
+  validates :name, :city, :order_ahead, :employee_count, presence: true
   has_many :pies
 end
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Shop, type: :model do
   describe 'relationships' do
     it { should have_many :pies }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:city)}
+    it { should validate_presence_of(:order_ahead)}
+    it { should validate_presence_of(:employee_count)}
+  end
 end
