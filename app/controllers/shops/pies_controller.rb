9a83c1d3874a08f29d666f000705344cb627ba17
Nick Sacco <nicksacco10@gmail.com>
2023-05-09 17:28:35 -0700

    complete us5

--- app/controllers/shops/pies_controller.rb
+++ app/controllers/shops/pies_controller.rb
@@ -1,5 +1,6 @@
 class Shops::PiesController < ApplicationController
   def index
-    
+    @shop = Shop.find(params[:id])
+    @pies = @shop.pies
   end
 end
--- spec/features/shops/pies/index_spec.rb
+++ spec/features/shops/pies/index_spec.rb
@@ -8,16 +8,40 @@ RSpec.describe "Shop Pie's index Page", type: :feature do
   describe "When I visit '/shops/:parent_id/pies'" do
     it "shows each pie that is associated with this shop" do
       visit "/shops/#{@dub.id}/pies"
-
+      
       expect(page).to have_content(@dub.name)
       expect(page).to have_content(@mince.name)
       expect(page).to have_content(@mush.name)
       expect(page).to have_content(@curry.name)
       expect(page).to_not have_content(@spin.name)
     end
+    
+    it "shows each pies attributes" do
+      visit "/shops/#{@dub.id}/pies"
 
-    xit "shows each pies attributes" do
+      within "#pies_#{@mince.id}" do
+        expect(page).to have_content(@mince.name)
+        expect(page).to have_content(@mince.category)
+        expect(page).to have_content(@mince.wholesale)
+        expect(page).to have_content(@mince.bake_time)
+        expect(page).to_not have_content(@spin.name)
+      end
+      
+      within "#pies_#{@mush.id}" do
+        expect(page).to have_content(@mush.name)
+        expect(page).to have_content(@mush.category)
+        expect(page).to have_content(@mush.wholesale)
+        expect(page).to have_content(@mush.bake_time)
+        expect(page).to_not have_content(@spin.name)
+      end
 
+      within "#pies_#{@curry.id}" do
+        expect(page).to have_content(@curry.name)
+        expect(page).to have_content(@curry.category)
+        expect(page).to have_content(@curry.wholesale)
+        expect(page).to have_content(@curry.bake_time)
+        expect(page).to_not have_content(@spin.name)
+      end
     end
   end
 end
