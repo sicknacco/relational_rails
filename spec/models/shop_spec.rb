697ec7fae09f7ac3247dc00ff2d40adbc8b6ed71
Nick Sacco <nicksacco10@gmail.com>
2023-05-09 19:15:50 -0700

    test: shops_by_timestamp class method

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
+  before(:each) do
+    test_data
+  end
+
   describe 'relationships' do
     it { should have_many :pies }
   end
 
-  describe 'validations' do
-    it { should validate_presence_of(:name)}
-    it { should validate_presence_of(:city)}
-    it { should validate_presence_of(:order_ahead)}
-    it { should validate_presence_of(:employee_count)}
+  # describe 'validations' do
+  #   it { should validate_presence_of(:name)}
+  #   it { should validate_presence_of(:city)}
+  #   it { should validate_presence_of(:order_ahead)}
+  #   it { should validate_presence_of(:employee_count)}
+  # end
+
+  describe "class methods" do
+    describe "#shops_by_timestamp" do
+      it "orders shops by most recently created first" do
+        
+        expect(Shop.shops_by_timestamp).to eq([@flushing, @truck, @dub])
+      end
+    end
   end
 end
