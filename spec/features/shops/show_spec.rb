4b5bc289ef6a430b5368148c590f5e2f8e1ebb7a
Nick Sacco <nicksacco10@gmail.com>
2023-05-09 19:42:22 -0700

    complete us7 pies count added to shops show page

--- spec/features/shops/show_spec.rb
+++ spec/features/shops/show_spec.rb
@@ -9,11 +9,21 @@ RSpec.describe "Shop's show page", type: :feature do
     describe "I see the shop with that id" do
       it "shows attributes of a specific shop" do
         visit "/shops/#{@dub.id}"
-
+        
         expect(page).to have_content(@dub.name)
         expect(page).to have_content(@dub.city)
         expect(page).to have_content(@dub.order_ahead)
         expect(page).to have_content(@dub.employee_count)
+      end
+      
+      it 'can show the number of pie varieties at this shop' do
+        visit "/shops/#{@dub.id}"
+
+        expect(page).to have_content("Pie Varieties Available: 3")
+
+        visit "/shops/#{@truck.id}"
+
+        expect(page).to have_content("Pie Varieties Available: 4")
       end
     end
   end
