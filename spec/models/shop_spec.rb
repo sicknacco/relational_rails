48e78caebc95998fd87fe0e66039022ac4666407
Nick Sacco <nicksacco10@gmail.com>
2023-05-09 17:28:20 -0700

    Add validations and tests to models

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Shop, type: :model do
     it { should have_many :pies }
   end
 
-  # describe 'validations' do
-  #   it { should validate_presence_of(:name)}
-  #   it { should validate_presence_of(:city)}
-  #   it { should validate_presence_of(:order_ahead)}
-  #   it { should validate_presence_of(:employee_count)}
-  # end
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:city)}
+    it { should validate_presence_of(:order_ahead)}
+    it { should validate_presence_of(:employee_count)}
+  end
 end
