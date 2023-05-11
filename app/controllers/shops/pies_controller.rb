e5356b4eeff96368b84e52ebe05f70da62bb391b
Nick Sacco <nicksacco10@gmail.com>
2023-05-10 19:25:09 -0700

    feat: added shop pie create route and controller action

--- app/controllers/shops/pies_controller.rb
+++ app/controllers/shops/pies_controller.rb
@@ -5,5 +5,22 @@ class Shops::PiesController < ApplicationController
   end
 
   def new
+    @shop = Shop.find(params[:id])
+  end
+
+  def create
+    @shop = Shop.find(params[:id])
+    @pie = @shop.pies.new(pie_params)
+    if @pie.save
+      redirect_to "/shops/#{@shop.id}/pies"
+    else
+      redirect_to "/shops/#{@shop.id}/pies/new"
+      flash[:alert] = "Error: You must complete all fields"
+    end
+  end
+
+  private
+  def pie_params
+    params.permit(:id, :name, :category, :bake_time, :wholesale)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   get '/shops/new', to: 'shops#new'
   post '/shops', to: 'shops#create'
   get '/shops/:id', to: 'shops#show'
-  get '/shops/:id/pies', to: 'shops/pies#index'
-  get '/shops/:id/pies/new', to: 'shops/pies#new'
   get '/shops/:id/edit', to: 'shops#edit'
   patch '/shops/:id', to: 'shops#update'
+  
+  get '/shops/:id/pies', to: 'shops/pies#index'
+  get '/shops/:id/pies/new', to: 'shops/pies#new'
+  post '/shops/:id/pies', to: 'shops/pies#create'
 
   get 'pies', to: 'pies#index'
   get 'pies/:id', to: 'pies#show'
