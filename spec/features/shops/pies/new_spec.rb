367f316a743a8173ad38bc02bb595bc2711b7050
Nick Sacco <nicksacco10@gmail.com>
2023-05-10 19:07:28 -0700

    test: tests for new pie form

--- spec/features/shops/pies/new_spec.rb
+++ spec/features/shops/pies/new_spec.rb
@@ -5,5 +5,29 @@ RSpec.describe "Shop's new pie form" do
     test_data
   end
 
-  
+  describe "Shop's pie creation form" do
+    describe "It can create a new pie for a specific shop" do
+      it 'can verify form elements' do
+        visit "/shops/#{@dub.id}/pies/new"
+        
+        expect(page).to have_field('name')
+        expect(page).to have_field('category')
+        expect(page).to have_field('bake_time')
+        expect(page).to have_unchecked_field
+        expect(page).to have_button("Create Pie")
+      end
+      
+      it "fills in a form to create a new pie for a shop" do
+        visit "/shops/#{@dub.id}/pies/new"
+        fill_in(:name, with: 'Egg and Cheese')
+        fill_in(:category, with: 'Breakfast')
+        fill_in(:bake_time, with: 30)
+        check(:wholesale)
+        click_button("Create Pie")
+
+        expect(current_path).to eq("/shops/#{@dub.id}/pies")
+        expect(page).to have_content("Egg and Cheese")
+      end
+    end
+  end
 end
